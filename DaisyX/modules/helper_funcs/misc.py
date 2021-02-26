a92e28482fa397de3d9557b73c9986944a0f1cfb
InukaAsith <inukaasith7@gmail.com>
2021-02-26 19:04:00 +0000

    pylint: auto fixes

--- DaisyX/modules/helper_funcs/misc.py
+++ DaisyX/modules/helper_funcs/misc.py
@@ -1,8 +1,9 @@
 from typing import Dict, List
 
-from DaisyX import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
+
+from DaisyX import NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
