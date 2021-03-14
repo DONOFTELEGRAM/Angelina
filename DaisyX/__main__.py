23b117440c1bc21d7a1600ac1b992333772447a4
InukaAsith <inukaasith7@gmail.com>
2021-03-14 04:07:26 +0000

    pylint: auto fixes

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -296,9 +296,7 @@ def help_button(update, context):
         if mod_match:
             module = mod_match.group(1)
             text = (
-                "Help  for  {}  module \n".format(
-                    HELPABLE[module].__mod_name__
-                )
+                "Help  for  {}  module \n".format(HELPABLE[module].__mod_name__)
                 + HELPABLE[module].__help__
             )
             query.message.edit_text(
